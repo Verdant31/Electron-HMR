70e0819c1cc5877dd9b6488cddfdc6a292c80ce8
verdant31 <joao.pedro3110@hotmail.com>
2022-11-04 17:24:30 -0300

    first commit

---
+++ src/renderer/App.tsx
@@ -0,0 +1,22 @@
+import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+
+const Hello = () => {
+  return (
+    <div className="container">
+      <h1>React HMR (Hand Move Recognition) </h1>
+      <h3>Initial Guide</h3>
+      <div />
+    </div>
+  );
+};
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Hello />} />
+      </Routes>
+    </Router>
+  );
+}
